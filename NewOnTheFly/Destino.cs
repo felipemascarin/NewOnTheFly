bba89c8e2c03a5642dc47560010dc2e210b4c6ca
Felipe Mascarin <felipe_mascarin@hotmail.com>
2022-09-27 00:43:49 -0300

    Adicionar arquivos de projeto.

---
+++ NewOnTheFly/Destino.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewOnTheFly
+{
+    internal class Destino
+    {
+        public string IATA { get; set; }
+        public string Continente { get; set; }
+        public string Pais { get; set; }
+        public string Cidade { get; set; }
+        public string Nome_Aeroporto { get; set; }
+        public DateTime Tempo_Voo { get; set; }
+
+        //public Destino() { }
+
+        public Destino(string iata, string continente, string pais, string cidade, string nome_aeroporto, DateTime tempo_voo)
+        {
+            IATA = iata;
+            Continente = continente;
+            Pais = pais;
+            Cidade = cidade;
+            Nome_Aeroporto = nome_aeroporto;
+            Tempo_Voo = tempo_voo;
+        }
+
+
+    }
+}
---
+++ NewOnTheFly/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Data.SqlClient;
+
+namespace NewOnTheFly
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+
+
+
+
+
+
+        }
+    }
+}
